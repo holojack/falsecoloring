420f2f1bbecec86b6d3a5afbf6edc2d59596d445
Jared Petersen <jpetersen11@wou.edu>
2016-05-23 13:53:23 -0700

    First attempt at coloring

--- FalseColoring/FalseColoring/Program.cs
+++ FalseColoring/FalseColoring/Program.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics; // For the stopwatch
 using System.Threading.Tasks; // For the Parallel
 using System.Linq; // For the 2D array comparison
+using System.Drawing; // For the color
+using System.Drawing.Imaging; // For the image output
 
 namespace FalseColoring
 {
@@ -12,8 +14,8 @@ namespace FalseColoring
         {
             // Set up the grid dimensions
             // Parallel will be slower for smaller grids
-            int width = 12;
-            int length = 12;
+            int width = 500;
+            int length = 500;
 
             // Declare the 2D array
             int[,] grid = new int[length, width];
@@ -30,21 +32,9 @@ namespace FalseColoring
             sw.Stop();
             Console.WriteLine("Elapsed time taken sequentially: {0}", sw.ElapsedMilliseconds);
 
-            // Print the results
-            for (int row = 0; row < width; row++)
-            {
-                for (int col = 0; col < length; col++)
-                {
-                    if (col == length - 1)
-                    {
-                        System.Console.WriteLine(grid[col, row]);
-                    }
-                    else
-                    {
-                        System.Console.Write(grid[col, row] + "-");
-                    }
-                }
-            }
+            // Output the results
+            //PrintGrid(grid);
+            OutputImage(grid, "image1.bmp"); // Image stored in the bin/Debug folder of the project directory
 
             // Fill the 2D array with the starting values
             SetupGrid(grid);
@@ -56,21 +46,9 @@ namespace FalseColoring
             sw.Stop();
             Console.WriteLine("Elapsed time taken parallel: {0}", sw.ElapsedMilliseconds);
 
-            // Print the results
-            for (int row = 0; row < width; row++)
-            {
-                for (int col = 0; col < length; col++)
-                {
-                    if (col == length - 1)
-                    {
-                        System.Console.WriteLine(grid[col, row]);
-                    }
-                    else
-                    {
-                        System.Console.Write(grid[col, row] + "-");
-                    }
-                }
-            }
+            // Output the results
+            //PrintGrid(grid);
+            OutputImage(grid, "image2.bmp"); // Image stored in the bin/Debug folder of the project directory
 
             // Program is done, make sure to the let user see the results
             System.Console.WriteLine("Press Any Key to Exit");
@@ -285,6 +263,158 @@ namespace FalseColoring
         {
             // Find the temperature by averaging the four grids around it
             return (left + top + right + bottom) / 4;
+        }
+
+        /*
+         * AssignColor
+         * 
+         * Assigns a color to the given node temperature
+         * 
+         * Parameters:
+         * int temp - The node temperature
+         * 
+         * Returns: The corresponding color
+         */
+        static Color AssignColor(int temp)
+        {
+            double[] colorVals = new double[3];
+
+            // Color map is as follows:
+            // 0    -> (0, 0, 128)
+            // 500  -> (0, 255, 0)
+            // 1000 -> (255, 255, 0)
+            // 1500 -> (255, 128, 0)
+            // 2000 -> (255, 0, 0)
+            // Source: http://dsp.stackexchange.com/a/4679
+
+            // If the temp is not on the map, use linear interpolation to 
+            // determine the color for the temperature
+            if (temp == 0)
+            {
+                colorVals[0] = 0;
+                colorVals[1] = 0;
+                colorVals[2] = 128;
+            }
+            else if (temp == 500)
+            {
+                colorVals[0] = 0;
+                colorVals[1] = 255;
+                colorVals[2] = 0;
+            }
+            else if (temp == 1000)
+            {
+                colorVals[0] = 255;
+                colorVals[1] = 255;
+                colorVals[2] = 0;
+            }
+            else if (temp == 1500)
+            {
+                colorVals[0] = 255;
+                colorVals[1] = 128;
+                colorVals[2] = 0;
+            }
+            else if (temp == 2000)
+            {
+                colorVals[0] = 255;
+                colorVals[1] = 0;
+                colorVals[2] = 0;
+            }
+            else
+            {
+                int[] leftRGB = new int[3];
+                int[] rightRGB = new int[3];
+                double multiplier = 0.0;
+
+                // Determine the left and right RGB values for the calculation
+                if (temp > 0 && temp < 500)
+                {
+                    leftRGB[0] = 0;
+                    leftRGB[1] = 0;
+                    leftRGB[2] = 128;
+                    rightRGB[0] = 0;
+                    rightRGB[1] = 255;
+                    rightRGB[2] = 0;
+                    multiplier = 0.25;
+                }
+                else if (temp > 500 && temp < 1000)
+                {
+                    leftRGB[0] = 0;
+                    leftRGB[1] = 255;
+                    leftRGB[2] = 0;
+                    rightRGB[0] = 255;
+                    rightRGB[1] = 255;
+                    rightRGB[2] = 0;
+                    multiplier = 0.5;
+                }
+                else if (temp > 1000 && temp < 1500)
+                {
+                    leftRGB[0] = 255;
+                    leftRGB[1] = 255;
+                    leftRGB[2] = 0;
+                    rightRGB[0] = 255;
+                    rightRGB[1] = 128;
+                    rightRGB[2] = 0;
+                    multiplier = 0.75;
+                }
+                else if (temp > 1500 && temp < 2000)
+                {
+                    leftRGB[0] = 255;
+                    leftRGB[1] = 128;
+                    leftRGB[2] = 0;
+                    rightRGB[0] = 255;
+                    rightRGB[1] = 0;
+                    rightRGB[2] = 0;
+                    multiplier = 1.0;
+                }
+
+                // Assign the colors
+                for (int i = 0; i < 3; i++ )
+                {
+                    colorVals[i] = ((double) temp / 2000.0) * (double) leftRGB[i] / multiplier + ((2000.0 - (double) temp) / 2000.0) * rightRGB[i];
+
+                    // Make sure the colors don't exceed the accepted ranges
+                    if (colorVals[i] > 255.0) colorVals[i] = 255.0;
+                }
+            }
+
+            return Color.FromArgb((int) Math.Ceiling(colorVals[0]), (int) Math.Ceiling(colorVals[1]), (int) Math.Ceiling(colorVals[2]));
+        }
+
+        static void PrintGrid(int[,] grid)
+        {
+            // Print the grid
+            for (int row = 0; row < grid.GetLength(1); row++)
+            {
+                for (int col = 0; col < grid.GetLength(0); col++)
+                {
+                    if (col == grid.GetLength(0) - 1)
+                    {
+                        System.Console.WriteLine(grid[col, row]);
+                    }
+                    else
+                    {
+                        System.Console.Write(grid[col, row] + "-");
+                    }
+                }
+            }
+        }
+
+        static void OutputImage(int[,] grid, string imageName)
+        {
+            // Set up the image
+            Bitmap bitmap = new Bitmap(grid.GetLength(0), grid.GetLength(1));
+
+            // Add the pixels to the image
+            for (int row = 0; row < grid.GetLength(1); row++)
+            {
+                for (int col = 0; col < grid.GetLength(0); col++)
+                {
+                    bitmap.SetPixel(col, row, AssignColor(grid[col, row]));
+                }
+            }
+
+            // Save it
+            bitmap.Save(imageName);
         }
     }
 }
